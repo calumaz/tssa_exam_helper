c6b8204ca0214145edb00b6c4322ccdfcba7fc6b
Calum McBride <calum.mcbride@gmail.com>
2023-02-05 19:33:30 -0500

    did a bunch of work on quizbank

--- lib/models/quiz_bank.dart
+++ lib/models/quiz_bank.dart
@@ -1,11 +1,53 @@
+import 'dart:math';
 import 'package:tssa_exam_helper/quiz_questions/four_A_Questions.dart';
+import 'package:tssa_exam_helper/widgets/answer_button.dart';
 
 class QuizBank {
   QuizBank({required this.title, required this.quizQuestions}) {
+    rng = Random();
     quizQuestions.shuffle();
+    answerButtons = generateAnswerButtons();
   }
   String title;
   List<Question> quizQuestions;
+  List<AnswerButton> answerButtons = [];
+  int currentQuestionNumber = 0;
+  dynamic rng;
+
+  // shuffle the order in answer buttons are returned and increments the currentQuestionNumber
+  List<AnswerButton> generateAnswerButtons() {
+    Question question = quizQuestions[currentQuestionNumber];
+    List<AnswerButton> currentAnswerButtons = [];
+    int numberOfAnswers = question.wrongAnswers.length;
+    int staggerAmount = rng.nextInt(numberOfAnswers + 1);
+
+    question.wrongAnswers.shuffle();
+
+    for (int i = 0; i < staggerAmount; i++) {
+      currentAnswerButtons.add(AnswerButton(
+        text: question.wrongAnswers[i],
+        isCorrectAnswer: false,
+        nextQuestion: () {},
+      ));
+    }
+    // insert correct answer
+    currentAnswerButtons.add(AnswerButton(
+      text: question.correctAnswer,
+      isCorrectAnswer: true,
+      nextQuestion: () {},
+    ));
+    for (int i = staggerAmount; i < numberOfAnswers; i++) {
+      currentAnswerButtons.add(AnswerButton(
+        text: question.wrongAnswers[i],
+        isCorrectAnswer: false,
+        nextQuestion: () {},
+      ));
+    }
+
+    // increment the current question before returning list of buttons
+    currentQuestionNumber++;
+    return currentAnswerButtons;
+  }
 }
 
 class Question {
